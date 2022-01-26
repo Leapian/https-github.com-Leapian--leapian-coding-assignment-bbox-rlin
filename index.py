f003197057a84dc79e955a164573221a5ec0cfd1
naseweisssss <ryanlin.x.w@hotmail.co.uk>
2022-01-26 23:23:58 +0000

    Fourth Commit for Fifth requirement

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 # import dataset data
 from dataset import data
+import numpy as np
+import statistics
 # your code here
 # let x_coordinate be x, y_coordinate be y, text be text, word height be height
 class Word:
@@ -30,9 +32,48 @@ for item in word_dict:
 
 temp = [(len(value), key) for key, value in height_dict.items()]
 print(max(temp)[1])
-
-
+main_word_dict = height_dict[max(temp)[1]]
+ycoordinate_dict = {}
+for item in word_dict:
     
+    if item.y not in ycoordinate_dict:
+        ycoordinate_dict[item.y] = [item]
+
+    else:
+        ycoordinate_dict[item.y].append(item)
+
+print(ycoordinate_dict)
+
+position_lst = [key for key, value in ycoordinate_dict.items()]
+position_lst = sorted(position_lst)
+vert_dist = []
+for i in range(1, len(position_lst)):
+    vert_dist.append((position_lst[i])- position_lst[i-1] - max(temp)[1])
+
+
+
+print("The mean of the vertical distance is" + ' ' + str(np.mean(vert_dist)))
+print("The mode of the vertical distance is" + ' ' + str(statistics.mode(vert_dist)))
+print("The median of the vertical distance is" + ' ' + str(np.median(vert_dist)))
+
+def give_x(elem):
+    return elem.x
+for key in ycoordinate_dict:
+  ycoordinate_dict[key] = sorted(ycoordinate_dict[key], key = give_x)
+
+print(ycoordinate_dict)
+
+dist_lst = []
+for key in ycoordinate_dict:
+    tuple_list = []
+    for word in ycoordinate_dict[key]:
+        tuple_list.append((word.x, word.width))
+    for i in range(1, len(tuple_list)):
+        dist_lst.append(tuple_list[i][0]- tuple_list[i-1][0] - tuple_list[i-1][1])
+    
+print("The distance between words is" + ' ' + str(np.mean(dist_lst)))
+print("The distance between words is" + ' ' + str(statistics.mode(dist_lst)))
+print("The distance between words is" + ' ' + str(np.median(dist_lst)))
 
     
 
