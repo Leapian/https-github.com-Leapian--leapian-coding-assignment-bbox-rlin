392f76338d69a80dcf00689a82ce14915fc4765d
naseweisssss <ryanlin.x.w@hotmail.co.uk>
2022-01-26 22:31:50 +0000

    Second commit for 3rd requirement

--- index.py
+++ index.py
@@ -14,10 +14,25 @@ word_dict = []
 del data[0]
 
 for i in range(len(data)):
-    word = Word(data[i][3], data[i][4], data[i][7], data[i][6], data[i][5])
+    word = Word(float(data[i][3]), float(data[i][4]), data[i][7], round(float(data[i][6])), float(data[i][5]))
     word_dict.append(word)
 
-print(word_dict)
+
+height_dict = {}
+
+for item in word_dict:
+    
+    if item.height not in height_dict:
+        height_dict[item.height] = [item]
+
+    else:
+        height_dict[item.height].append(item)
+
+print(height_dict)
+
+    
+
+    
 
 
     
